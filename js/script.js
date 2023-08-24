205517bbadfb37f76ec5d8ff2d3651cfab39c44d
BambangSatmoto <bambangsatmoto9@gmail.com>
2023-08-25 04:35:15 +0700

    Menambahkan about

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.querySelector("#hamburger-menu").onclick = () => {
 };
 
 // klik diluar sidebar untuk menghilangkan nav
-const hamburger = documen.querySelector("#hamburger-menu");
+const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
