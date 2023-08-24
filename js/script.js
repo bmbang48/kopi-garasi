8068b819792b7e00d445176e97bdc5d928b0a119
BambangSatmoto <bambangsatmoto9@gmail.com>
2023-08-25 02:22:58 +0700

    Inisialisasi Awal

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+  console.log("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = documen.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
